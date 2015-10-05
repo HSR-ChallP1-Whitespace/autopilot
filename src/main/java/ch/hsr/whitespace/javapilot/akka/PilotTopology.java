37f882295fbd758521894c748aa345167468a8d7
Stefan Kapferer <stefan@kapferer.ch>
2015-10-05 19:07:07 +0200

    Refactored PilotTopology

--- src/main/java/ch/hsr/whitespace/javapilot/akka/PilotTopology.java
+++ src/main/java/ch/hsr/whitespace/javapilot/akka/PilotTopology.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import ch.hsr.whitespace.javapilot.akka.experimental.PowerUpUntilPenalty;
 
 /**
  * creates the complete topology and provides a Map of well-defined entry-points
@@ -17,17 +16,16 @@ public class PilotTopology {
 	public static final String PENALTY_ENTRYPOINT = "PENALTY_ENTRYPOINT";
 
 	private final ActorSystem system;
-	private final ActorRef kobayashi;
+	private final ActorRef pilot;
 	private final Map<String, ActorRef> entryPoints = new HashMap<>();
 
-	public PilotTopology(ActorRef kobayashi, ActorSystem system) {
-		this.kobayashi = kobayashi;
+	public PilotTopology(ActorRef pilot, ActorSystem system) {
+		this.pilot = pilot;
 		this.system = system;
 	}
 
 	public Map<String, ActorRef> create() {
-
-		ActorRef initialProcessor = system.actorOf(PowerUpUntilPenalty.props(kobayashi, 1500));
+		ActorRef initialProcessor = system.actorOf(WhiteSpacePilot.props(pilot, 1500));
 
 		entryPoints.put(PENALTY_ENTRYPOINT, initialProcessor);
 		entryPoints.put(SENSOR_ENTRYPOINT, initialProcessor);
